2f5fa59c164b7584b443b978953ccf4e736258a3
aadielpr <aadielpr.work@yahoo.com>
2025-12-25 11:28:06 +0700

    d5 part 2

--- 2015/d5.c
+++ 2015/d5.c
@@ -2,48 +2,42 @@
 #include <stdio.h>
 #include <string.h>
 
-enum Vowel { A = 'a', I = 'i', U = 'u', E = 'e', O = 'o' };
-
 int main() {
     char str[17];
     int nice_string = 0;
 
     while (scanf("%16s", str) == 1) {
-        int vowel_count = 0;
-        bool twin_letter = false;
-        bool illegal = false;
-        printf("%s\n", str);
-
         int len = strlen(str);
-        for (int i = 0; i < len; i++) {
+        int seen[26][26];
+        memset(seen, -1, sizeof(seen));
+
+        bool squeezed = false;
+        bool paired = false;
+
+        for (int i = 0; i < len - 1; i++) {
             char c = str[i];
-            char next_c = i == len - 1 ? '0' : str[i + 1];
+            char n = str[i + 1];
+            int x = c - 'a';
+            int y = n - 'a';
 
-            // concat the string
-            char pair_str[3];
-            snprintf(pair_str, sizeof(pair_str), "%c%c", c, next_c);
-
-            // check vowel
-            if (c == A || c == I || c == U || c == E || c == O) {
-                vowel_count++;
+            if (i + 2 < len && c == str[i + 2]) {
+                squeezed = true;
             }
 
-            // check is this twin
-            if (c == next_c) {
-                twin_letter = true;
+            if (seen[x][y] != -1 && i - seen[x][y] > 1) {
+                paired = true;
             }
 
-            // check illegal string
-            if (strcmp(pair_str, "ab") == 0 || strcmp(pair_str, "cd") == 0 ||
-                strcmp(pair_str, "pq") == 0 || strcmp(pair_str, "xy") == 0) {
-                illegal = true;
+            if (squeezed && paired) {
+                nice_string++;
+                break;
             }
-        }
 
-        if (vowel_count >= 3 && twin_letter && !illegal) {
-            nice_string++;
+            if (seen[x][y] == -1) {
+                seen[x][y] = i;
+            }
         }
     }
 
-    printf("total: %d\n", nice_string);
+    printf("%d\n", nice_string);
 }
